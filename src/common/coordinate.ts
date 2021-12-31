a525ae67f4602151026de364059738ce3f3c3f70
Rob March <robertmarch5@gmail.com>
2021-12-31 17:16:56 +0000

    Improve day 20 run speed

--- src/common/coordinate.ts
+++ src/common/coordinate.ts
@@ -1,16 +1,9 @@
 export class BaseCoordinate {
-  public static addCoordinates(
-    a: BaseCoordinate,
-    b: BaseCoordinate
-  ): BaseCoordinate {
-    return new BaseCoordinate(
-      a.x + b.x,
-      a.y + b.y
-    );
+  public static addCoordinates(a: BaseCoordinate, b: BaseCoordinate): BaseCoordinate {
+    return new BaseCoordinate(a.x + b.x, a.y + b.y);
   }
 
-  constructor(public x: number, public y: number) {
-  }
+  constructor(public x: number, public y: number) {}
 
   public equals(other: BaseCoordinate): boolean {
     return this.x === other.x && this.y === other.y;
@@ -20,34 +13,19 @@ export class BaseCoordinate {
 export class Base3DCoordinate {
   public static fromArray(coordinates: number[]): Base3DCoordinate {
     if (coordinates.length !== 3) {
-      throw new Error(
-        `Unexpected number of coordinates given: expected 3, got ${coordinates.length}`
-      );
+      throw new Error(`Unexpected number of coordinates given: expected 3, got ${coordinates.length}`);
     }
     return new Base3DCoordinate(coordinates[0], coordinates[1], coordinates[2]);
   }
 
   constructor(public x: number, public y: number, public z: number) {}
 
-  public getVectorFromOtherCoordinate(
-    other: Base3DCoordinate
-  ): Base3DCoordinate {
-    return new Base3DCoordinate(
-      this.x - other.x,
-      this.y - other.y,
-      this.z - other.z
-    );
+  public getVectorFromOtherCoordinate(other: Base3DCoordinate): Base3DCoordinate {
+    return new Base3DCoordinate(this.x - other.x, this.y - other.y, this.z - other.z);
   }
 
-  public static addCoordinates(
-    a: Base3DCoordinate,
-    b: Base3DCoordinate
-  ): Base3DCoordinate {
-    return new Base3DCoordinate(
-      a.x + b.x,
-      a.y + b.y,
-      a.z + b.z
-    );
+  public static addCoordinates(a: Base3DCoordinate, b: Base3DCoordinate): Base3DCoordinate {
+    return new Base3DCoordinate(a.x + b.x, a.y + b.y, a.z + b.z);
   }
 
   public equals(other: Base3DCoordinate): boolean {
@@ -55,6 +33,6 @@ export class Base3DCoordinate {
   }
 
   public getManhattanMagnitude(): number {
-    return Math.abs(this.x) + Math.abs(this.y) + Math.abs(this.z)
+    return Math.abs(this.x) + Math.abs(this.y) + Math.abs(this.z);
   }
 }
--- src/days/day20.ts
+++ src/days/day20.ts
@@ -13,71 +13,92 @@ interface GridInformation {
   infiniteGridValues: string;
 }
 
+class Coordinate extends BaseCoordinate {
+  public static addCoordinates(a: Coordinate, b: Coordinate): Coordinate {
+    return new Coordinate(a.x + b.x, a.y + b.y);
+  }
+
+  private _serialisedValue: string;
+
+  constructor(public x: number, public y: number) {
+    super(x, y);
+    this._serialisedValue = `${x},${y}`;
+  }
+
+  public get serialisedValue(): string {
+    return this._serialisedValue;
+  }
+}
+
 export class Day20 extends Day {
   public solvePartOne(input: string): number {
-    const parts: string[] = splitLines(input, true, 2);
-    const enhancementKey: string = parts[0];
-    const infiniteGridSwitches: boolean = enhancementKey[0] == "#";
-
-    let lightPixels: BaseCoordinate[] = this._getInitialLightPixels(parts[1])
-    const gridInfo: GridInformation = this._getInitialGridInformation(lightPixels, infiniteGridSwitches);
-
-    range(0, 2).forEach(() => {
-      this._updateGridInformation(gridInfo);
-
-      const newLightPixels: BaseCoordinate[] = [];
-      for (let x = gridInfo.minX - 1; x <= gridInfo.maxX + 1; x++) {
-        for (let y = gridInfo.minY - 1; y <= gridInfo.maxY + 1; y++) {
-          if (enhancementKey[this._getPixelValue(x, y, lightPixels, gridInfo)] == "#") {
-            newLightPixels.push(new BaseCoordinate(x, y));
-          }
-        }
-      }
-      lightPixels = newLightPixels;
-    });
-
-    return lightPixels.length;
+    return this._getPixelCountAfterNEnhancements(input, 2);
   }
 
   public solvePartTwo(input: string): number {
+    return this._getPixelCountAfterNEnhancements(input, 10);
+  }
+
+  private _getPixelCountAfterNEnhancements(input: string, enhancementCount: number): number {
     const parts: string[] = splitLines(input, true, 2);
     const enhancementKey: string = parts[0];
     const infiniteGridSwitches: boolean = enhancementKey[0] == "#";
 
-    let lightPixels: BaseCoordinate[] = this._getInitialLightPixels(parts[1])
+    let lightPixels: Record<string, Coordinate> = this._getInitialLightPixels(parts[1]);
     const gridInfo: GridInformation = this._getInitialGridInformation(lightPixels, infiniteGridSwitches);
 
-    range(0, 10).forEach(() => {
+    range(0, enhancementCount).forEach(() => {
       this._updateGridInformation(gridInfo);
 
-      const newLightPixels: BaseCoordinate[] = [];
+      const newLightPixels: Record<string, Coordinate> = {};
       for (let x = gridInfo.minX - 1; x <= gridInfo.maxX + 1; x++) {
         for (let y = gridInfo.minY - 1; y <= gridInfo.maxY + 1; y++) {
-          if (enhancementKey[this._getPixelValue(x, y, lightPixels, gridInfo)] == "#") {
-            newLightPixels.push(new BaseCoordinate(x, y));
+          const pixel: Coordinate = new Coordinate(x, y);
+          if (enhancementKey[this._getPixelValue(pixel, lightPixels, gridInfo)] == "#") {
+            newLightPixels[pixel.serialisedValue] = pixel;
           }
         }
       }
       lightPixels = newLightPixels;
     });
 
-    return lightPixels.length;
+    return Object.keys(lightPixels).length;
   }
 
-  private _getInitialLightPixels(initialPixels: string): BaseCoordinate[] {
-    return splitLines(initialPixels).flatMap((line, lineIndex) =>
+  private _getInitialLightPixels(initialPixels: string): Record<string, Coordinate> {
+    const coords = splitLines(initialPixels).flatMap((line, lineIndex) =>
       line
         .split("")
-        .map((value, valueIndex) => (value === "#" ? new BaseCoordinate(valueIndex, lineIndex) : null))
+        .map((value, valueIndex) => (value === "#" ? new Coordinate(valueIndex, lineIndex) : null))
         .filter((val) => !!val)
     );
+
+    return coords.reduce((map: Record<string, Coordinate>, currentCoord) => {
+      map[currentCoord.serialisedValue] = currentCoord;
+      return map;
+    }, {});
   }
 
-  private _getInitialGridInformation(lightPixels: BaseCoordinate[], infiniteGridSwitches: boolean): GridInformation {
-    const minX: number = lightPixels.reduce((prevMin, curr) => (curr.x < prevMin ? curr.x : prevMin), 1000000);
-    const maxX: number = lightPixels.reduce((prevMax, curr) => (curr.x > prevMax ? curr.x : prevMax), -1000000);
-    const minY: number = lightPixels.reduce((prevMin, curr) => (curr.y < prevMin ? curr.y : prevMin), 1000000);
-    const maxY: number = lightPixels.reduce((prevMax, curr) => (curr.y > prevMax ? curr.y : prevMax), -1000000);
+  private _getInitialGridInformation(
+    lightPixels: Record<string, Coordinate>,
+    infiniteGridSwitches: boolean
+  ): GridInformation {
+    const minX: number = Object.values(lightPixels).reduce(
+      (prevMin, curr) => (curr.x < prevMin ? curr.x : prevMin),
+      1000000
+    );
+    const maxX: number = Object.values(lightPixels).reduce(
+      (prevMax, curr) => (curr.x > prevMax ? curr.x : prevMax),
+      -1000000
+    );
+    const minY: number = Object.values(lightPixels).reduce(
+      (prevMin, curr) => (curr.y < prevMin ? curr.y : prevMin),
+      1000000
+    );
+    const maxY: number = Object.values(lightPixels).reduce(
+      (prevMax, curr) => (curr.y > prevMax ? curr.y : prevMax),
+      -1000000
+    );
 
     return {
       minX,
@@ -97,19 +118,23 @@ export class Day20 extends Day {
     gridInfo.infiniteGridValues = !gridInfo.infiniteGridSwitches || gridInfo.infiniteGridValues === "1" ? "0" : "1";
   }
 
-  private _getPixelValue(x: number, y: number, allLightPixels: BaseCoordinate[], gridInfo: GridInformation): number {
+  private _getPixelValue(
+    pixel: Coordinate,
+    allLightPixels: Record<string, Coordinate>,
+    gridInfo: GridInformation
+  ): number {
     let binaryString: string = "";
 
     for (let dy = -1; dy <= 1; dy++) {
       for (let dx = -1; dx <= 1; dx++) {
-        const coord = new BaseCoordinate(x + dx, y + dy);
+        const coord: Coordinate = Coordinate.addCoordinates(pixel, new Coordinate(dx, dy));
         if (
           coord.x >= gridInfo.minX &&
           coord.x <= gridInfo.maxX &&
           coord.y >= gridInfo.minY &&
           coord.y <= gridInfo.maxY
         ) {
-          binaryString += allLightPixels.some((pixel) => pixel.equals(coord)) ? "1" : "0";
+          binaryString += !!allLightPixels[coord.serialisedValue] ? "1" : "0";
         } else {
           binaryString += gridInfo.infiniteGridValues;
         }
