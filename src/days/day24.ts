b80f948ee2ab9d1132b3178f33bd6f4bfc58c462
Rob March <robertmarch5@gmail.com>
2022-01-09 11:59:51 +0000

    Rewrite and solve day 24

--- src/days/day24.ts
+++ src/days/day24.ts
@@ -2,124 +2,71 @@ import { Day } from "../day";
 import { range } from "../utils/array-utils";
 import { splitLines } from "../utils/string-utils";
 
-enum InstructionType {
-  INPUT = "inp",
-  ADD = "add",
-  MULTIPLY = "mul",
-  DIVIDE = "div",
-  MODULO = "mod",
-  EQUAL = "eql",
-}
-
-interface VariableState {
-  w: number;
-  x: number;
-  y: number;
-  z: number;
-}
-
-type Variable = keyof VariableState;
-
-class Instruction {
-  public static deserialise(line: string): Instruction {
-    const parts: string[] = line.split(" ");
-    const instruction = new Instruction(parts[0] as InstructionType, parts[1] as Variable, null);
-    if (parts.length > 2) {
-      if (parts[2].match(/-?\d+/)) {
-        instruction.variable2 = parseInt(parts[2]);
-      } else {
-        instruction.variable2 = parts[2] as Variable;
-      }
-    }
-
-    return instruction;
-  }
-
-  constructor(
-    public instructionType: InstructionType,
-    public variable1: Variable,
-    public variable2: Variable | number
-  ) {}
-
-  public process(state: VariableState): void {
-    const var2Value = this._evaluateVariable2(state);
-    switch (this.instructionType) {
-      case InstructionType.INPUT:
-        throw new Error("Inputs should be handled separately");
-      case InstructionType.ADD:
-        state[this.variable1] = state[this.variable1] + this._evaluateVariable2(state);
-        break;
-      case InstructionType.MULTIPLY:
-        state[this.variable1] = state[this.variable1] * this._evaluateVariable2(state);
-        break;
-      case InstructionType.DIVIDE:
-        state[this.variable1] = Math.floor(state[this.variable1] / this._evaluateVariable2(state));
-        break;
-      case InstructionType.MODULO:
-        state[this.variable1] = state[this.variable1] % this._evaluateVariable2(state);
-        break;
-      case InstructionType.EQUAL:
-        state[this.variable1] = state[this.variable1] === this._evaluateVariable2(state) ? 1 : 0;
-        break;
-    }
-  }
-
-  private _evaluateVariable2(state: VariableState): number {
-    if (typeof this.variable2 === "number") {
-      return this.variable2 as number;
-    } else {
-      return state[this.variable2];
-    }
-  }
-}
+const InputBlockRegex =
+  /inp w\nmul x 0\nadd x z\nmod x 26\ndiv z (\d+)\nadd x (-?\d+)\neql x w\neql x 0\nmul y 0\nadd y 25\nmul y x\nadd y 1\nmul z y\nmul y 0\nadd y w\nadd y (-?\d+)\nmul y x\nadd z y/g;
 
 export class Day24 extends Day {
   public solvePartOne(input: string): number {
-    const instructions: Instruction[] = splitLines(input).map((line) => Instruction.deserialise(line));
-    let variableStates: Record<number, VariableState> = { 0: { w: 0, x: 0, y: 0, z: 0 } };
-
-    instructions.forEach((instruction) => {
-      if (instruction.instructionType === InstructionType.INPUT) {
-        const previousInputs: number[] = Object.keys(variableStates)
-          .map((val) => parseInt(val))
-          .sort((a, b) => b - a);
-        const previousZStates: Set<number> = new Set();
-        const newVariableStates: Record<number, VariableState> = {};
-
-        previousInputs.forEach((partialModelNumber) => {
-          const state: VariableState = variableStates[partialModelNumber];
-
-          if (!previousZStates.has(state.z)) {
-            previousZStates.add(state.z);
-            range(1, 10).forEach((i) => {
-              newVariableStates[partialModelNumber * 10 + i] = {
-                x: state.x,
-                y: state.y,
-                z: state.z,
-                w: i,
-              };
-            });
-          }
-        });
-
-        variableStates = newVariableStates;
-      } else {
-        Object.values(variableStates).forEach((variableState) => instruction.process(variableState));
-      }
-    });
-
-    return Math.max(
-      ...Object.keys(variableStates)
-        .map((val) => parseInt(val))
-        .filter((modelNumber) => variableStates[modelNumber].z === 0)
-    );
+    return this._solve(input, true);
   }
 
   public solvePartTwo(input: string): number {
-    return -1;
+    return this._solve(input, false);
   }
 
-  private _areStatesEqualBeforeInput(state1: VariableState, state2: VariableState): boolean {
-    return state1.z === state2.z;
+  private _solve(input: string, solveForMaximum: boolean): number {
+    const inputBlocks: number[][] = [...input.matchAll(InputBlockRegex)].map((block) =>
+      block.slice(1).map((val) => parseInt(val))
+    );
+
+    let partialModelNumbersToZValues: Record<number, number> = { 0: 0 };
+
+    inputBlocks.forEach((block: number[], blockIndex: number) => {
+      const previousPartialModelNumbers: number[] = Object.keys(partialModelNumbersToZValues)
+        .map((val) => parseInt(val))
+        .sort((a, b) => (solveForMaximum ? b - a : a - b));
+      const previousZStates: Set<number> = new Set();
+      const newPartialModelNumbersToZValues: Record<number, number> = {};
+
+      previousPartialModelNumbers.forEach((partialModelNumber) => {
+        const zValue: number = partialModelNumbersToZValues[partialModelNumber];
+
+        if (!previousZStates.has(zValue)) {
+          previousZStates.add(zValue);
+          if (zValue <= this._calculateRemainingZDivisor(inputBlocks, blockIndex)) {
+            range(1, 10).forEach((i) => {
+              newPartialModelNumbersToZValues[partialModelNumber * 10 + i] = this._calculateFinalZValueAfterBlock(
+                zValue,
+                i,
+                block
+              );
+            });
+          }
+        }
+      });
+
+      partialModelNumbersToZValues = newPartialModelNumbersToZValues;
+    });
+
+    const validModelNumbers: number[] = Object.keys(partialModelNumbersToZValues)
+      .map((val) => parseInt(val))
+      .filter((modelNumber) => partialModelNumbersToZValues[modelNumber] === 0);
+    return solveForMaximum ? Math.max(...validModelNumbers) : Math.min(...validModelNumbers);
+  }
+
+  /**
+   * Calculates the final z value after a block of code, assumes input matches the same operations.
+   */
+  private _calculateFinalZValueAfterBlock(initialZ: number, inputW: number, block: number[]): number {
+    const x1: number = (initialZ % 26) + block[1] != inputW ? 1 : 0;
+    const z1: number = Math.floor(initialZ / block[0]);
+    return (25 * x1 + 1) * z1 + (inputW + block[2]) * x1;
+  }
+
+  /**
+   * Calculates the maximum Z value that could be reduced to a 0 from all blocks after the current block (inclusive).
+   */
+  private _calculateRemainingZDivisor(blocks: number[][], currentIndex: number): number {
+    return blocks.slice(currentIndex).reduce((total, curr) => total * curr[0], 1) - 1;
   }
 }
